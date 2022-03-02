9884c04a309c05f5c7e4727c19bc3889728a8f48
J.T. Sage <jtsage+datebox@gmail.com>
2022-03-01 22:12:24 -0500

    Bugfix: text was still visible when HUD is hidden

--- src/ProductionInspector.lua
+++ src/ProductionInspector.lua
@@ -344,7 +344,7 @@ function ProductionInspector:draw()
 			linesPerEntry = linesPerEntry - 1
 		end
 
-		if #info_text == 0 or not g_productionInspector.isEnabledVisible or g_sleepManager:getIsSleeping()  then
+		if #info_text == 0 or not g_productionInspector.isEnabledVisible or g_sleepManager:getIsSleeping() or g_noHudModeEnabled then
 			-- we have no entries, hide the overlay and leave
 			-- also if we hid it on purpose
 			self.inspectBox:setVisible(false)
